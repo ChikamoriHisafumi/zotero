e8c40d20e53a53310ba1b9122b79b7dfc130c2d3
chikamori <chikamori@nibiohn.go.jp>
2020-05-26 16:51:37 +0900

    option - startsWith (underbar) - was added

--- common.js
+++ common.js
@@ -243,91 +243,19 @@ function main() {
 
 		});
 
-		// show_01 = $('#show_01').prop('checked');
-		// show_02 = $('#show_02').prop('checked');
-		// show_03 = $('#show_03').prop('checked');
-		// show_04 = $('#show_04').prop('checked');
-		// show_05 = $('#show_05').prop('checked');
-		// show_06 = $('#show_06').prop('checked');
-		// show_07 = $('#show_07').prop('checked');
-		// show_08 = $('#show_08').prop('checked');
-		// show_09 = $('#show_09').prop('checked');
-		// show_10 = $('#show_10').prop('checked');
-
-		// bold_01 = $('#bold_01').prop('checked');
-		// bold_02 = $('#bold_02').prop('checked');
-		// bold_03 = $('#bold_03').prop('checked');
-		// bold_04 = $('#bold_04').prop('checked');
-		// bold_05 = $('#bold_05').prop('checked');
-		// bold_06 = $('#bold_06').prop('checked');
-		// bold_07 = $('#bold_07').prop('checked');
-		// bold_08 = $('#bold_08').prop('checked');
-		// bold_09 = $('#bold_09').prop('checked');
-		// bold_10 = $('#bold_10').prop('checked');
-		//
-		// italic_01 = $('#italic_01').prop('checked');
-		// italic_02 = $('#italic_02').prop('checked');
-		// italic_03 = $('#italic_03').prop('checked');
-		// italic_04 = $('#italic_04').prop('checked');
-		// italic_05 = $('#italic_05').prop('checked');
-		// italic_06 = $('#italic_06').prop('checked');
-		// italic_07 = $('#italic_07').prop('checked');
-		// italic_08 = $('#italic_08').prop('checked');
-		// italic_09 = $('#italic_09').prop('checked');
-		// italic_10 = $('#italic_10').prop('checked');
-		//
-		// underline_01 = $('#underline_01').prop('checked');
-		// underline_02 = $('#underline_02').prop('checked');
-		// underline_03 = $('#underline_03').prop('checked');
-		// underline_04 = $('#underline_04').prop('checked');
-		// underline_05 = $('#underline_05').prop('checked');
-		// underline_06 = $('#underline_06').prop('checked');
-		// underline_07 = $('#underline_07').prop('checked');
-		// underline_08 = $('#underline_08').prop('checked');
-		// underline_09 = $('#underline_09').prop('checked');
-		// underline_10 = $('#underline_10').prop('checked');
-		//
-		// prefix_01 = $('[name="prefix_01"]').val();
-		// prefix_02 = $('[name="prefix_02"]').val();
-		// prefix_03 = $('[name="prefix_03"]').val();
-		// prefix_04 = $('[name="prefix_04"]').val();
-		// prefix_05 = $('[name="prefix_05"]').val();
-		// prefix_06 = $('[name="prefix_06"]').val();
-		// prefix_07 = $('[name="prefix_07"]').val();
-		// prefix_08 = $('[name="prefix_08"]').val();
-		// prefix_09 = $('[name="prefix_09"]').val();
-		// prefix_10 = $('[name="prefix_10"]').val();
-		//
-		// suffix_01 = $('[name="suffix_01"]').val();
-		// suffix_02 = $('[name="suffix_02"]').val();
-		// suffix_03 = $('[name="suffix_03"]').val();
-		// suffix_04 = $('[name="suffix_04"]').val();
-		// suffix_05 = $('[name="suffix_05"]').val();
-		// suffix_06 = $('[name="suffix_06"]').val();
-		// suffix_07 = $('[name="suffix_07"]').val();
-		// suffix_08 = $('[name="suffix_08"]').val();
-		// suffix_09 = $('[name="suffix_09"]').val();
-		// suffix_10 = $('[name="suffix_10"]').val();
-		//
-		// delimiter_01 = $('[name="delimiter_01"]').val();
-		// delimiter_02 = $('[name="delimiter_02"]').val();
-		// delimiter_03 = $('[name="delimiter_03"]').val();
-		// delimiter_04 = $('[name="delimiter_04"]').val();
-		// delimiter_05 = $('[name="delimiter_05"]').val();
-		// delimiter_06 = $('[name="delimiter_06"]').val();
-		// delimiter_07 = $('[name="delimiter_07"]').val();
-		// delimiter_08 = $('[name="delimiter_08"]').val();
-		// delimiter_09 = $('[name="delimiter_09"]').val();
-		// delimiter_10 = $('[name="delimiter_10"]').val();
-
-		// console.log(manual_tags.join('; '));
-
 		let set_Manual_Tags = new Set(Manual_Tags.join('; ').split('; '));
 
 		// console.log(set_Manual_Tags);
 
 		var html_manual_tags = '';
-		var no = 1;
+		var no = 0;
+
+		html_manual_tags += '<div>';
+		html_manual_tags += '<label for="chk_underbar">';
+		html_manual_tags += '<input type="checkbox" value="chk_underbar" id="chk_underbar"' + (chk_underbar ? ' checked="checked"' : '') + '>';
+		html_manual_tags += 'アンダーバーで始まるタグ以外も表示する';
+		html_manual_tags += '</label>';
+		html_manual_tags += '<div>';
 
 		html_manual_tags += '<div>';
 		html_manual_tags += '<label for="chk_tagall">';
@@ -342,20 +270,29 @@ function main() {
 		// このFor文だと、フォーマッターが働かないみたい。仕方なく別のFor文に変更。
 		// for(let v of set_Manual_Tags.values()){
 
-		let array_Manual_Tags = Array.from(set_Manual_Tags.values()).sort();
-		var tmp_Manual_Tags = [];
+		var array_Manual_Tags = array_Manual_Tags = Array.from(set_Manual_Tags.values()).sort();
 
-		for (var j = 0; j < array_Manual_Tags.length; j++) {
+		if (!chk_underbar) {
 
-			if (array_Manual_Tags[j].startsWith('_')) {
+			// array_Manual_Tags = Array.from(set_Manual_Tags.values()).sort();
+			//
+			// } else {
 
-				tmp_Manual_Tags.push(array_Manual_Tags[j]);
+			var tmp_Manual_Tags = [];
+
+			for (var j = 0; j < array_Manual_Tags.length; j++) {
+
+				if (array_Manual_Tags[j].startsWith('_')) {
+
+					tmp_Manual_Tags.push(array_Manual_Tags[j]);
+
+				}
 
 			}
 
-		}
+			array_Manual_Tags = tmp_Manual_Tags;
 
-		array_Manual_Tags = tmp_Manual_Tags;
+		}
 
 		// console.log(array_Manual_Tags);
 
@@ -368,17 +305,27 @@ function main() {
 		//
 		// console.log(chk_tags);
 
-		// var rm = 0;
-		//
-		// while (array_Manual_Tags[rm] !== '') {
-		//
-		// rm++;
-		//
-		// }
-		//
-		// // console.log(rm);
-		//
-		// array_Manual_Tags.splice(rm, 1);
+		var rm = 0;
+
+		while (array_Manual_Tags[rm] !== '') {
+
+			rm++;
+
+			if (rm == array_Manual_Tags.length) {
+
+				break;
+
+			}
+
+		}
+
+		// console.log(rm);
+
+		if (rm !== array_Manual_Tags.length) {
+
+			array_Manual_Tags.splice(rm, 1);
+
+		}
 
 		// cl(array_Manual_Tags);
 
@@ -398,8 +345,8 @@ function main() {
 
 			if (array_Manual_Tags[i] !== '') {
 
-				html_manual_tags += '<label for="chk_tags_' + no + '">';
-				html_manual_tags += '<input type="checkbox" value="chk_tags_' + no + '" id="chk_tags_' + no + '"' + (chk_tags[i] ? 'checked="checked"' : '') + '>';
+				html_manual_tags += '<label for="chk_tags_' + (no + 1) + '">';
+				html_manual_tags += '<input type="checkbox" value="chk_tags_' + (no + 1) + '" id="chk_tags_' + (no + 1) + '"' + (chk_tags[no + 1] ? 'checked="checked"' : '') + '>';
 				html_manual_tags += array_Manual_Tags[i];
 				html_manual_tags += '</label>';
 
@@ -433,7 +380,7 @@ function main() {
 
 					// console.log(set_Manual_Tags.indexOf(_Manual_Tags[j]));
 
-					class_name += '_' + (array_Manual_Tags.indexOf(_Manual_Tags[j]) + 0) + '_';
+					class_name += '_' + (array_Manual_Tags.indexOf(_Manual_Tags[j]) + 1) + '_';
 
 				}
 
@@ -459,6 +406,8 @@ function main() {
 			}
 
 		}
+
+		// cl(array_Manual_Tags);
 
 		// }
 
@@ -488,8 +437,9 @@ function main() {
 
 		$('[id^="chk_tags"]').each(function(index, element) {
 
-			chk_tags[i] = $(this).prop('checked');
-			i++;
+			var chk_no = parseInt($(this).attr('id').split('_')[2]);
+			chk_tags[chk_no] = $(this).prop('checked');
+			// i++;
 
 		});
 
@@ -520,7 +470,7 @@ function main() {
 
 			// console.log(c_name);
 
-			for (var i = 0; i < chk_tags.length; i++) {
+			for (var i = 1; i <= chk_tags.length; i++) {
 
 				if (chk_tags[i]) {
 
@@ -574,24 +524,35 @@ function main() {
 
 	}
 
+	// cl(chk_tags);
+
 	// for debug
 
-	cl('/////////////////////////////////////////////');
-
-	cl(show, 1);
-	cl(bold, 1);
-	cl(italic, 1);
-	cl(underline, 1);
-	cl(prefix, 1);
-	cl(suffix, 1);
-	cl(delimiter, 1);
-	cl(alignment, 1);
-
-	cl('/////////////////////////////////////////////');
+	// cl('/////////////////////////////////////////////');
+	//
+	// cl(show, 1);
+	// cl(bold, 1);
+	// cl(italic, 1);
+	// cl(underline, 1);
+	// cl(prefix, 1);
+	// cl(suffix, 1);
+	// cl(delimiter, 1);
+	// cl(alignment, 1);
+	//
+	// cl('/////////////////////////////////////////////');
 
 }
 
 $(function() {
+
+	$(document).on('click', '#chk_underbar', function() {
+
+		chk_underbar = $(this).prop('checked');
+		chk_tags = [];
+
+		main();
+
+	});
 
 	$(document).on('click', '#chk_tagall', function() {
 
@@ -656,13 +617,13 @@ $(function() {
 
 		case 0:
 
-			show = [ null, false, true, true, true, false, false, false, false, false, false, false, false ];
-			bold = [ null, false, false, false, false, false, false, false, false, false, false, false, false ];
-			italic = [ null, false, false, false, false, false, false, false, false, false, false, false, false ];
-			underline = [ null, false, false, false, false, false, false, false, false, false, false, false, false ];
-			prefix = [ null, '', '', '', '', '', '', '', '', '', '', '' ];
-			suffix = [ null, '', '', '', '', '', '', '', '', '', '', '' ];
-			delimiter = [ null, '', '', '', '', '', '', '', '', '', '', '' ];
+			show = [ empty, false, true, true, true, false, false, false, false, false, false, false, false ];
+			bold = [ empty, false, false, false, false, false, false, false, false, false, false, false, false ];
+			italic = [ empty, false, false, false, false, false, false, false, false, false, false, false, false ];
+			underline = [ empty, false, false, false, false, false, false, false, false, false, false, false, false ];
+			prefix = [ empty, '', '', '', '', '', '', '', '', '', '', '' ];
+			suffix = [ empty, '', '', '', '', '', '', '', '', '', '', '' ];
+			delimiter = [ empty, '', '', '', '', '', '', '', '', '', '', '' ];
 			alignment = [ __tr_01__, __tr_02__, __tr_03__, __tr_04__, __tr_05__, __tr_06__, __tr_07__, __tr_08__, __tr_09__, __tr_10__, __tr_11__, __tr_12__ ];
 
 			break;
@@ -782,11 +743,11 @@ $(function() {
 
 		if ($(this).prop('checked')) {
 
-			chk_tags[i - 1] = true;
+			chk_tags[i] = true;
 
 		} else {
 
-			chk_tags[i - 1] = false;
+			chk_tags[i] = false;
 
 		}
 
@@ -943,6 +904,7 @@ var prefix = [];
 var suffix = [];
 var delimiter = [];
 
+var chk_underbar = false;
 var chk_tagall = false;
 var chk_tags = [];
 
@@ -1034,7 +996,11 @@ var Committee = [];
 var History = [];
 var Legislative_Body = [];
 
-var DEBUG_MODE_01 = true;
+// //////////////////////////////////////////////////
+
+var DEBUG_MODE_01 = false;
+
+// //////////////////////////////////////////////////
 
 function cl(msg, dm) {
 
